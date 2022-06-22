4e2e9356bf77858a2cd596d72b43906689a0c7dd
DeepakmaniRaj30 <98530550+DeepakmaniRaj30@users.noreply.github.com>
2022-06-23 01:05:02 +0530

    Created PositiveNumbers.py
    This program runs all positive numbers in range

---
+++ PositiveNumbers.py
@@ -0,0 +1,17 @@
+list1 = [12, -7, 5, 64, -14]
+num = 0
+print(" Positive Numbers in  list1:")
+while(num < len(list1)):
+    if list1[num] >= 0:
+        print(list1[num], end = " ")
+    num += 1
+
+
+list2 = [12, 14, -95, 3]
+print()
+num = 0
+print(" Positive Numbers in  list2:")
+while(num < len(list2)):
+    if list2[num] >= 0:
+        print(list2[num], end = " ")
+    num += 1
